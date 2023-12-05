e32fa175023d550b96859a7a0b9f009f695054e8
Stijn Koppen <s.koppen@tudelft.nl>
2023-12-05 14:00:38 +0100

    Documentation of black-box optimizer

--- 2023/code/black_box_opt.py
+++ 2023/code/black_box_opt.py
@@ -2,18 +2,25 @@ import numpy as np
 from scipy.optimize import minimize, Bounds, LinearConstraint
 from responses import objective
 
-n = 20
-x0 = np.random.rand(n)
+# Initial design
+x0 = np.random.rand(10)
 
+# Set global variable bounds
 bounds = Bounds(np.zeros_like(x0), np.ones_like(x0))
+
+# Setup linear constraint
 linear_constraint = LinearConstraint(np.ones_like(x0), -np.inf, 0.5 * x0.size)
 
 # In case your constraint is nonlinear, use
 # nonlinear_constraint = NonlinearConstraint(g, -np.inf, 0.5 * x0.size, jac=dg)
 # with g, dg the function calls to constraint value and sensitivities
 
-opt = {'verbose': 1, 'maxiter': 100, 'disp': True}
-res = minimize(objective, x0, method='trust-constr', jac=True,
-               constraints=[linear_constraint], bounds=bounds, tol=1e-6, options= opt)
+# Set some options for the optimizer
+opt = {'verbose': 2, 'maxiter': 100, 'disp': True}
 
-print(res.x)
+# Solve the nonlinear minimization problem
+res = minimize(objective, x0, jac=True, method='trust-constr',
+               constraints=linear_constraint, bounds=bounds,
+               tol=1e-6, options=opt)
+
+print(f'Solution = {res.x}')
