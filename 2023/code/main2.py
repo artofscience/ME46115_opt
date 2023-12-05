f8e199625255d71e7f414798dc72b72698e9ec41
Stijn Koppen <s.koppen@tudelft.nl>
2023-12-05 10:53:41 +0100

    modified main2 to directly take the objective function

--- 2023/code/main2.py
+++ 2023/code/main2.py
@@ -1,27 +1,19 @@
 import numpy as np
 from scipy.optimize import minimize, Bounds, LinearConstraint
-from analysis import analysis
-from objective import objective
+from responses import objective
 
+n = 20
+x0 = np.random.rand(n)
 
-def model(x):
-    u, Ke = analysis(x)
-    return objective(x, u, Ke)
+bounds = Bounds(np.zeros_like(x0), np.ones_like(x0))
+linear_constraint = LinearConstraint(np.ones_like(x0), -np.inf, 0.5 * x0.size)
 
+# In case your constraint is nonlinear, use
+# nonlinear_constraint = NonlinearConstraint(g, -np.inf, 0.5 * x0.size, jac=dg)
+# with g, dg the function calls to constraint value and sensitivities
 
-if __name__ == "__main__":
-    n = 20
-    x0 = np.random.rand(n)
+opt = {'verbose': 1, 'maxiter': 100, 'disp': True}
+res = minimize(objective, x0, method='trust-constr', jac=True,
+               constraints=[linear_constraint], bounds=bounds, tol=1e-6, options= opt)
 
-    bounds = Bounds(np.zeros_like(x0), np.ones_like(x0))
-    linear_constraint = LinearConstraint(np.ones_like(x0), -np.inf, 0.5 * x0.size)
-
-    # In case your constraint is nonlinear, use
-    # nonlinear_constraint = NonlinearConstraint(g, -np.inf, 0.5 * x0.size, jac=dg)
-    # with g, dg the function calls to constraint value and sensitivities
-
-    opt = {'verbose': 1, 'maxiter': 100, 'disp': True}
-    res = minimize(model, x0, method='trust-constr', jac=True,
-                   constraints=[linear_constraint], bounds=bounds, tol=1e-6, options= opt)
-
-    print(res.x)
+print(res.x)
